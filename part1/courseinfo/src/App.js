717acde21fbcf26ca0586286895f7bf5ed9a40f9
Moudi Kawi <kawimoudi@gmail.com>
2022-05-14 13:27:09 +0100

    Add solution to Part 1 - 1.4 Course Information exercise

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -29,8 +29,8 @@ const Total = (props) => {
 	return (
 		<p>
 			<strong>Number of Exercises: </strong>
-			{props.exercises.reduce(
-				(accumulator, current) => accumulator + current,
+			{props.parts.reduce(
+				(accumulator, current) => accumulator + current.exercises,
 				0
 			)}
 		</p>
@@ -39,24 +39,26 @@ const Total = (props) => {
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = {
-		name: "Fundamentals of React",
-		exercises: 10,
-	};
-	const part2 = {
-		name: "Using props to pass data",
-		exercises: 7,
-	};
-	const part3 = {
-		name: "State of a component",
-		exercises: 14,
-	};
+	const parts = [
+		{
+			name: "Fundamentals of React",
+			exercises: 10,
+		},
+		{
+			name: "Using props to pass data",
+			exercises: 7,
+		},
+		{
+			name: "State of a component",
+			exercises: 14,
+		},
+	];
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content parts={[part1, part2, part3]} />
-			<Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+			<Content parts={parts} />
+			<Total parts={parts} />
 		</div>
 	);
 };
