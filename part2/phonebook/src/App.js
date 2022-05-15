61779aabdf3409f9d058fb925b9e21b75988d4ca
Moudi Kawi <kawimoudi@gmail.com>
2022-05-15 19:28:24 +0100

    Add solution to Part 2 - 2.9 The Phonebook exercise

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "555-578347343" },
+		{ name: "Arto Hellas", number: "040-123456", id: 1 },
+		{ name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+		{ name: "Dan Abramov", number: "12-43-234345", id: 3 },
+		{ name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
 	]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
+	const [search, setSearch] = useState("");
 
 	function submitForm(e) {
 		e.preventDefault();
@@ -23,6 +27,26 @@ const App = () => {
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			Filter shown with:{" "}
+			<input
+				type="search"
+				onChange={(e) => setSearch(e.target.value)}
+				value={search}
+			/>
+			{search === ""
+				? null
+				: persons
+						.filter((person) =>
+							person.name.toLowerCase().includes(search.toLowerCase())
+						)
+						.map((person) => {
+							return (
+								<li key={person.name}>
+									{person.name} : {person.number}
+								</li>
+							);
+						})}
+			<h2>Add new</h2>
 			<form>
 				<div>
 					name:{" "}
