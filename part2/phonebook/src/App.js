3fd9e9ffc61837f3f874e125dc982b0eca5d781a
Moudi Kawi <kawimoudi@gmail.com>
2022-05-15 19:16:33 +0100

    Add solution to Part 2 - 2.8 The Phonebook exercise

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,16 +1,20 @@
 import { useState } from "react";
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", number: "555-578347343" },
+	]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	function submitForm(e) {
 		e.preventDefault();
 		if (!persons.some((person) => person.name === newName)) {
 			setPersons((prevState) => {
-				return [...prevState, { name: newName }];
+				return [...prevState, { name: newName, number: newNumber }];
 			});
 			setNewName("");
+			setNewNumber("");
 		} else {
 			alert(`${newName} is already added to Phonebook`);
 		}
@@ -25,6 +29,13 @@ const App = () => {
 					<input onChange={(e) => setNewName(e.target.value)} value={newName} />
 				</div>
 				<div>
+					number:{" "}
+					<input
+						onChange={(e) => setNewNumber(e.target.value)}
+						value={newNumber}
+					/>
+				</div>
+				<div>
 					<button type="submit" onClick={(e) => submitForm(e)}>
 						add
 					</button>
@@ -33,7 +44,11 @@ const App = () => {
 			<h2>Numbers</h2>
 			<ul>
 				{persons.map((person) => {
-					return <li key={person.name}>{person.name}</li>;
+					return (
+						<li key={person.name}>
+							{person.name} : {person.number}
+						</li>
+					);
 				})}
 			</ul>
 		</div>
