f40d86c919bcc1f85f1845fab20ad748ddde1be9
Moudi Kawi <kawimoudi@gmail.com>
2022-05-27 14:15:28 +0100

    Add solution to Part 3 - 3.20 + 3.21 Phonebook backend exercise

--- part3/phonebook/models/Entry.js
+++ part3/phonebook/models/Entry.js
@@ -14,7 +14,17 @@ mongoose
 
 const entrySchema = new mongoose.Schema({
 	name: { type: String, minLength: 3, required: true },
-	number: String,
+	number: {
+		type: String,
+		minLength: 8,
+		validate: {
+			validator: function (v) {
+				return /\d{2,3}-\d{5,15}/.test(v);
+			},
+			message: (props) => `${props.value} is not a valid phone number!`,
+		},
+		required: [true, "User phone number required"],
+	},
 });
 
 module.exports = mongoose.model("Entry", entrySchema);
