24caa666ec956da10c40a2e6c9839a83965a9aed
Moudi Kawi <kawimoudi@gmail.com>
2022-05-27 18:31:46 +0100

    Add solution to Part 4 - 4.4 Blog List exercise

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
 	return 1;
 };
 
+const totalLikes = (blogs) => {
+	return blogs.reduce((previousValue, current) => {
+		return previousValue + current.likes;
+	}, 0);
+};
+
 module.exports = {
 	dummy,
+	totalLikes,
 };
